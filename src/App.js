4ac67589f49ed5ba9fb9dc31d9b7fce7606d63dd
Mahadi Hasan <69779118+Mahadibappi@users.noreply.github.com>
2023-04-01 00:23:22 +0600

    todo created

--- src/App.js
+++ src/App.js
@@ -2,61 +2,60 @@ import { useEffect, useState } from "react";
 import ToDo from "./components/ToDo";
 import { addToDo, getAllToDo, updateToDo, deleteToDo } from "./utils/HandleApi";
 
-
 function App() {
-
-  const [toDo, setToDo] = useState([])
-  const [text, setText] = useState("")
-  const [isUpdating, setIsUpdating] = useState(false)
-  const [toDoId, setToDoId] = useState("")
+  const [toDo, setToDo] = useState([]);
+  const [text, setText] = useState("");
+  const [isUpdating, setIsUpdating] = useState(false);
+  const [toDoId, setToDoId] = useState("");
 
   useEffect(() => {
-    getAllToDo(setToDo)
-  }, [])
+    getAllToDo(setToDo);
+  }, []);
 
   const updateMode = (_id, text) => {
-    setIsUpdating(true)
-    setText(text)
-    setToDoId(_id)
-  }
+    setIsUpdating(true);
+    setText(text);
+    setToDoId(_id);
+  };
 
   return (
     <div className="App">
+      <div className="container bg-green-200 rounded-sm p-7">
+        <h1 className=" text-center mt-6 text-5xl p-5">ToDo App</h1>
 
-      <div className="container">
-
-        <h1>ToDo App</h1>
-
-        <div className="top">
+        <div className=" mt-6  flex justify-center gap-4 min-w-[500px]">
           <input
+            className=" text-center rounded-md w-[500px] p-4"
             type="text"
             placeholder="Add ToDos..."
             value={text}
             onChange={(e) => setText(e.target.value)}
           />
 
-          <div
-            className="add"
-            onClick={isUpdating ?
-              () => updateToDo(toDoId, text, setToDo, setText, setIsUpdating)
-              : () => addToDo(text, setText, setToDo)}>
+          <button
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+            onClick={
+              isUpdating
+                ? () =>
+                    updateToDo(toDoId, text, setToDo, setText, setIsUpdating)
+                : () => addToDo(text, setText, setToDo)
+            }
+          >
             {isUpdating ? "Update" : "Add"}
-          </div>
-
+          </button>
         </div>
 
         <div className="list">
-
-          {toDo.map((item) => <ToDo 
-          key={item._id} 
-          text={item.text}
-          updateMode = {() => updateMode(item._id, item.text)}
-          deleteToDo = {() => deleteToDo(item._id, setToDo)} />)}
-
+          {toDo.map((item) => (
+            <ToDo
+              key={item._id}
+              text={item.text}
+              updateMode={() => updateMode(item._id, item.text)}
+              deleteToDo={() => deleteToDo(item._id, setToDo)}
+            />
+          ))}
         </div>
-
       </div>
-
     </div>
   );
 }
--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -1,18 +1,18 @@
-import React from 'react'
+import React from "react";
 
-import {BiEdit} from "react-icons/bi"
-import {AiFillDelete} from "react-icons/ai"
+import { BiEdit } from "react-icons/bi";
+import { AiFillDelete } from "react-icons/ai";
 
-const ToDo = ({text, updateMode, deleteToDo}) => {
-    return (
-        <div className="todo">
-            <div className="text">{text}</div>
-            <div className="icons">
-                <BiEdit className='icon' onClick={updateMode} />
-                <AiFillDelete className='icon' onClick={deleteToDo} />
-            </div>
-        </div>
-    )
-}
+const ToDo = ({ text, updateMode, deleteToDo }) => {
+  return (
+    <div className="todo">
+      <div className="text">{text}</div>
+      <div className="icons">
+        <BiEdit className="icon" onClick={updateMode} />
+        <input type="checkbox" className="icon checkbox" onClick={deleteToDo} />
+      </div>
+    </div>
+  );
+};
 
-export default ToDo
+export default ToDo;
